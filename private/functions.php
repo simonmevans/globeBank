0399b5dd5de8a969ddd3684c52332b29bd41a080
Simon <simonmitchellevans@gmail.com>
2019-01-18 16:51:23 -0600

    initial

---
+++ private/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+function url_for($script_path) {
+  // add the leading '/' if not present
+  if($script_path[0] != '/') {
+    $script_path = "/" . $script_path;
+  }
+  return WWW_ROOT . $script_path;
+}
+
+function u ($string=""){
+  return urlencode($string);
+}
+
+function raw_u ($string=""){
+  return rawurlencode($string);
+}
+
+function h($string=""){
+  return htmlspecialchars ($string);
+}
+function error404 (){
+  header("HTTP/1.1 404 Not Found ");
+  exit();
+}
+function error500 (){
+  header($_SERVER['SERVER_PROTOCOL']. " 500 internal server error");
+  exit();
+}
+function redirectTo($location){
+  header('Location: '. $location);
+  exit();
+}
+?>
---
+++ public/staff/pages/show.php
@@ -0,0 +1,12 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+$id = isset($_GET['id']) ? $_GET['id'] : '1';
+?>
+<div id="content">
+     <a class="back-link" href="<?php echo url_for('/staff/pages/index.php')?>">&laquo;Back to List</a>
+     <div class="page show">
+          Page ID:
+          <?php echo h($id);?>
+     </div>
+</div>
